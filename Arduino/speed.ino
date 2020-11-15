4a2801144b5a645e50d1eba96f67103937440aa6
lekdev-7 <lekdevani007@gmail.com>
2020-11-15 16:25:17 +0530

    added arduino skecth(speedometer)

---
+++ Arduino/speed.ino
@@ -0,0 +1,23 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+int RX = 2, TX = 3;
+TinyGPSPlus gps;
+SoftwareSerial gpssoft(RX, TX);
+void setup()
+{
+  Serial.begin(9600);
+  gpssoft.begin(9600);
+}
+SoftwareSerial ss(2, 3);
+void loop()
+{
+  while (ss.available() > 0)
+   if (gps.encode(gpssoft.read())){
+     if (gps.location.isUpdated())
+  {
+    Serial.print("MPS="); 
+    Serial.println(gps.speed.mps());
+  }
+   }
+  delay(500);
+}  
